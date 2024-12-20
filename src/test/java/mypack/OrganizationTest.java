f032cf6018437fda5dcff4dee069008aff2c48b0
bindu <bindu@Shiva_9686>
2024-12-20 13:02:54 +0530

    first push..

---
+++ src/test/java/mypack/OrganizationTest.java
@@ -0,0 +1,15 @@
+package mypack;
+import org.testng.annotations.Test;
+
+public class OrganizationTest {
+@Test
+public void createOrg()
+{
+	System.out.println("createOrg1");
+}
+@Test
+public void modifyOrg()
+{
+	System.out.println("modifyOrg1");
+}
+}
